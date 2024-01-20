eee5b13f327149611bf6502b3b16bf2b75e169c4
MARTUSYS <jonk98@mail.ru>
2024-01-20 16:02:19 +1000

    Changed complexity from quadratic to approximately linear due to integration of KMP algorithm

--- 5.py
+++ 5.py
@@ -1,13 +1,41 @@
 with open("text.txt", "r", encoding="utf-8") as f:
     words = f.read().splitlines()
 
+
 input_word = input('Введите слово: ')
 
-for word in words:
-    for i in range(len(input_word) - 1, 1, -1):  # 1, len(input_word)
-        if i + 1 <= len(word) and input_word.endswith(word[:i]):
-            # print(i, input_word[i - 1:], word[:i])
-            print(input_word + word[i:])
+
+def prefix(s):
+    v = [0] * len(s)
+    for i in range(1, len(s)):
+        k = v[i - 1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k - 1]
+        if s[k] == s[i]:
+            k = k + 1
+        v[i] = k
+    return v
+
+
+def kmp(s, t):
+    # index = -1
+    f = prefix(s)
+    k = 0
+    for i in range(len(t)):
+        if k >= len(s):
+            k - 1
             break
+        while k > 0 and s[k] != t[i]:
+            k = f[k - 1]
+        if s[k] == t[i]:
+            k = k + 1
+        # if k == len(s):
+            # index = i + 1 - len(s)
+            # break
+    return k
 
 
+for i in words:
+    prefix_x = kmp(i, input_word)
+    if prefix_x and prefix_x < min(len(input_word), len(i)):
+        print(input_word + i[prefix_x:])
