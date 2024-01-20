1661fd610c379212df8594bc9c2f0e25bcf61212
MARTUSYS <jonk98@mail.ru>
2024-01-20 11:09:35 +1000

    Task 5 added

--- 5.py
+++ 5.py
@@ -1,1 +1,13 @@
+with open("text.txt", "r", encoding="utf-8") as f:
+    words = f.read().splitlines()
+
+input_word = input('Введите слово: ')
+
+for word in words:
+    for i in range(len(input_word) - 1, 1, -1):  # 1, len(input_word)
+        if i + 1 <= len(word) and input_word.endswith(word[:i]):
+            # print(i, input_word[i - 1:], word[:i])
+            print(input_word + word[i:])
+            break
+
 
